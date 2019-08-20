f35193ba9fd3cd44c61ef97ba0d93e852d04ed9f
hsh3030 <53160260+hsh3030@users.noreply.github.com>
2019-08-20 17:52:19 +0900

    s

--- tf/tf13_xor2.py
+++ tf/tf13_xor2.py
@@ -14,9 +14,41 @@ W1 = tf.Variable(tf.random_normal([2,100], name='weight'))
 b1 = tf.Variable(tf.random_normal([100]), name='bias')
 layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
 #=================================================================================
-W2 = tf.Variable(tf.random_normal([100,1], name='weight'))
-b2 = tf.Variable(tf.random_normal([1]), name='bias')
-hypothesis = tf.sigmoid(tf.matmul(layer1, W2) + b2)
+W2 = tf.Variable(tf.random_normal([100,50], name='weight'))
+b2 = tf.Variable(tf.random_normal([50]), name='bias')
+layer2 = tf.nn.relu(tf.matmul(layer1, W2) + b2)
+
+W3 = tf.Variable(tf.random_normal([50,100], name='weight'))
+b3 = tf.Variable(tf.random_normal([100]), name='bias')
+layer3 = tf.nn.relu(tf.matmul(layer2, W3) + b3)
+
+W4 = tf.Variable(tf.random_normal([100,75], name='weight'))
+b4 = tf.Variable(tf.random_normal([75]), name='bias')
+layer4 = tf.sigmoid(tf.matmul(layer3, W4) + b4)
+
+W5 = tf.Variable(tf.random_normal([75,100], name='weight'))
+b5 = tf.Variable(tf.random_normal([100]), name='bias')
+layer5 = tf.sigmoid(tf.matmul(layer4, W5) + b5)
+
+W6 = tf.Variable(tf.random_normal([100,100], name='weight'))
+b6 = tf.Variable(tf.random_normal([100]), name='bias')
+layer6 = tf.sigmoid(tf.matmul(layer5, W6) + b6)
+
+W7 = tf.Variable(tf.random_normal([100,100], name='weight'))
+b7 = tf.Variable(tf.random_normal([100]), name='bias')
+layer7 = tf.sigmoid(tf.matmul(layer6, W7) + b7)
+
+W8 = tf.Variable(tf.random_normal([100,100], name='weight'))
+b8 = tf.Variable(tf.random_normal([100]), name='bias')
+layer8 = tf.sigmoid(tf.matmul(layer7, W8) + b8)
+
+W9 = tf.Variable(tf.random_normal([100,100], name='weight'))
+b9 = tf.Variable(tf.random_normal([100]), name='bias')
+layer9 = tf.sigmoid(tf.matmul(layer8, W9) + b9)
+
+W10 = tf.Variable(tf.random_normal([100,1], name='weight'))
+b10 = tf.Variable(tf.random_normal([1]), name='bias')
+hypothesis = tf.sigmoid(tf.matmul(layer9, W10) + b10)
 
 cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
 train = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
@@ -29,7 +61,7 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
     for step in range(10001):
-        _, cost_val, w_val = sess.run([train, cost, W2], feed_dict={X: x_data, Y: y_data})
+        _, cost_val, w_val = sess.run([train, cost, W10], feed_dict={X: x_data, Y: y_data})
         
         if step % 100 == 0:
             print(step, cost_val, w_val)
